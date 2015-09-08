a143f0f0528622268fe202c1ea093804d26e7a29
d.kanehira <disc99.o@gmail.com>
2015-09-09 00:30:08 +0900

    Add ast classes

--- src/main/java/stone/StoneException.java
+++ src/main/java/stone/StoneException.java
@@ -1,13 +1,14 @@
 package stone;
 
+import stone.ast.ASTree;
+
 public class StoneException extends RuntimeException {
 
     public StoneException(String m) {
         super(m);
     }
 
-    // TODO
-//    public StoneException(String m, ASTree t) {
-//        super(m + " " + t.location());
-//    }
+    public StoneException(String m, ASTree t) {
+        super(m + " " + t.location());
+    }
 }
