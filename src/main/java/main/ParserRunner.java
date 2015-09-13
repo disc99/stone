86929479999ed9f55ba9d222e1bb30eb8e8e964d
d.kanehira <disc99.o@gmail.com>
2015-09-13 17:58:15 +0900

    Modify format

--- src/main/java/main/ParserRunner.java
+++ src/main/java/main/ParserRunner.java
@@ -1,11 +1,15 @@
 package main;
-import stone.ast.ASTree;
+
 import stone.*;
+import stone.ast.ASTree;
 
 public class ParserRunner {
+
     public static void main(String[] args) throws ParseException {
+
         Lexer l = new Lexer(new CodeDialog());
         BasicParser bp = new BasicParser();
+
         while (l.peek(0) != Token.EOF) {
             ASTree ast = bp.parse(l);
             System.out.println("=> " + ast.toString());
